1ba1142585e7106ceb61a03b87275d1ab2b52960
wfvvfw <wfvvfw@163.com>
2016-03-05 15:27:38 +0800

    add single layout new

---
+++ src/AbpCompanyName.AbpProjectName.WebMetronicGms/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AbpCompanyName.AbpProjectName.WebMetronicGms.Controllers
+{
+    public class AboutController : AbpProjectNameControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ src/AbpCompanyName.AbpProjectName.WebMetronicGms/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Abp.Web.Mvc.Authorization;
+
+namespace AbpCompanyName.AbpProjectName.WebMetronicGms.Controllers
+{
+    [AbpMvcAuthorize]
+    public class HomeController : AbpProjectNameControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
